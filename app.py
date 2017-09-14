ef979a0b4236fb1ecb29ed0900cc0416ed1c22ba
Justin Wong <justinwong12337@gmail.com>
2017-09-13 20:23:57 -0400

    Add virtualenv and other necessities

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
 import os
 
+url = os.getenv('DATABASE_URL')
+engine = create_engine('sqlite:///' + os.getenv('DATABASE_URL'))
+session = Session(engine)
+
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
-    return os.getenv("NAME")
+    return "hello"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run()
