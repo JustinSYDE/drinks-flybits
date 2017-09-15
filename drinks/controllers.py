bed0b11fa07d54e133908e86cca57cdee269d407
Justin Wong <justinwong12337@gmail.com>
2017-09-14 20:15:28 -0400

    Validation checks on input fields

--- drinks/controllers.py
+++ drinks/controllers.py
@@ -8,7 +8,7 @@ from drinks import (
 )
 from drinks.models.drink import Drink
 from flask import (
-    jsonify,
+    abort,
     request
 )
 
@@ -18,22 +18,43 @@ def find(id):
     drink = db.session.query(Drink).filter(Drink.id == id).first()
     return drink.name
 
+
 @app.route("/drink/search", methods=['GET'])
 def search():
     pass
 
-@app.route("/drink", methods=['POST'])
-def create():
+
+class Price:
+    def __init__(self, value):
+        try:
+            self.value = "{:.2f}".format(value)
+        except ValueError:
+            abort(400)
+
+
+class AvailabilityDate:
     # e.g. 30 nov 17
     datetime_format = "%d %b %y"
-    start_date_input = str(request.args.get('start_availability_date'))
-    end_date_input = str(request.args.get('end_availability_date')) if request.args.get('end_availability_date') else None
-    start_availability_date = datetime.strptime(start_date_input, datetime_format)
-    end_availability_date = datetime.strptime(end_date_input, datetime_format) if end_date_input else None
+
+    def __init__(self, date_str):
+        try:
+            self.value = datetime.strptime(date_str, self.datetime_format)
+        except ValueError:
+            abort(400)
+
+
+@app.route("/drink", methods=['POST'])
+def create():
+    name = str(request.args.get('name'))
+    price = Price(float(request.args.get('price')))
+    start_date_str = str(request.args.get('start_availability_date'))
+    end_date_str = str(request.args.get('end_availability_date')) if request.args.get('end_availability_date') else None
+    start_availability_date = AvailabilityDate(start_date_str)
+    end_availability_date = AvailabilityDate(end_date_str) if end_date_str else None
 
     drink = Drink(
-        str(request.args.get('name')),
-        float(request.args.get('price')),
+        name,
+        price.value,
         start_availability_date,
         end_availability_date
     )
@@ -41,6 +62,7 @@ def create():
     db.session.add(drink)
     db.session.commit()
     return 'success'
+
 
 @app.route("/write")
 def write():
